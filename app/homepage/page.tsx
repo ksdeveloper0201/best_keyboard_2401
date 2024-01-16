847968878a882102c0176ad0e7198043c68c769c
ksdeveloper0201 <ksdeveloper0201@gmail.com>
2024-01-16 16:32:40 +0000

    second

---
+++ app/homepage/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+function page() {
+    return (
+        <div>
+            <Button>hello</Button>
+            <Button>hello</Button>
+            <Button>hello</Button>
+        </div>
+    );
+}
+
+export default page;
---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <div className="py-8 px-10">
+            <div className="font-medium">BestKeyboard</div>
+            <div></div>
+        </div>
+    );
+}
+
+export default Header;

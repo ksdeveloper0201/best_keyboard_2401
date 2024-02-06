91fc4e7f9f6cf92388219b837768f794be9c265e
ksdeveloper0201 <ksdeveloper0201@gmail.com>
2024-02-06 16:32:24 +0000

    mod header

--- components/Header.tsx
+++ components/Header.tsx
@@ -34,43 +34,43 @@ import {
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
-const components: { title: string; href: string; description: string }[] = [
-    {
-        title: "Alert Dialog",
-        href: "/docs/primitives/alert-dialog",
-        description:
-            "A modal dialog that interrupts the user with important content and expects a response.",
-    },
-    {
-        title: "Hover Card",
-        href: "/docs/primitives/hover-card",
-        description:
-            "For sighted users to preview content available behind a link.",
-    },
-    {
-        title: "Progress",
-        href: "/docs/primitives/progress",
-        description:
-            "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-    },
-    {
-        title: "Scroll-area",
-        href: "/docs/primitives/scroll-area",
-        description: "Visually or semantically separates content.",
-    },
-    {
-        title: "Tabs",
-        href: "/docs/primitives/tabs",
-        description:
-            "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-    },
-    {
-        title: "Tooltip",
-        href: "/docs/primitives/tooltip",
-        description:
-            "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-    },
-];
+// const components: { title: string; href: string; description: string }[] = [
+//     {
+//         title: "Alert Dialog",
+//         href: "/docs/primitives/alert-dialog",
+//         description:
+//             "A modal dialog that interrupts the user with important content and expects a response.",
+//     },
+//     {
+//         title: "Hover Card",
+//         href: "/docs/primitives/hover-card",
+//         description:
+//             "For sighted users to preview content available behind a link.",
+//     },
+//     {
+//         title: "Progress",
+//         href: "/docs/primitives/progress",
+//         description:
+//             "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+//     },
+//     {
+//         title: "Scroll-area",
+//         href: "/docs/primitives/scroll-area",
+//         description: "Visually or semantically separates content.",
+//     },
+//     {
+//         title: "Tabs",
+//         href: "/docs/primitives/tabs",
+//         description:
+//             "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
+//     },
+//     {
+//         title: "Tooltip",
+//         href: "/docs/primitives/tooltip",
+//         description:
+//             "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
+//     },
+// ];
 
 function Header() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -78,66 +78,65 @@ function Header() {
         setIsMenuOpen(!isMenuOpen);
     }
 
-    const { setTheme } = useTheme();
 
     return (
         <>
-            <div id="first-header" className=" md:flex justify-between">
-                <div className="py-4 px-4  items-center justify-center flex relative">
-                    <FiMenu className="mr-auto cursor-pointer" onClick={toggleMenu} />
-                    <Link href={"/"} className="font-bold absolute text-center">
+            <div className="relative z-10">
+
+                <div className="flex justify-between items-center p-4">
+                    <FiMenu className="text-3xl cursor-pointer md:hidden" onClick={toggleMenu} />
+                    <Link href={"/"} className="font-bold text-xl text-center">
                         lifesaddle
                     </Link>
-                    <div className="ml-auto flex gap-4" >
-                        <IoSearchOutline />
-                        <FaUserCircle />
+                    <div className="flex gap-4" >
+                        <IoSearchOutline className="text-2xl" />
+                        <FaUserCircle className="text-3xl" />
                     </div>
                 </div>
-                {isMenuOpen && (
-                    <div className="absolute top-full left-0 bg-white shadow-md w-full">
-                        <ul className="list-none p-4">
-                            <li><Link href="/about-keyboards">キーボードについて</Link></li>
-                            <li><Link href="/ranking">ランキング</Link></li>
-                            <li><Link href="/search-keyboard">キーボードを探す</Link></li>
-                            <li><Link href="/favorites">お気に入り</Link></li>
-                        </ul>
+            </div>
+            <div className={`absolute top-12 left-0 h-full w-1/2 bg-white shadow-md transition-all ease-in-out duration-300 z-20 ${isMenuOpen ? 'translate-x-0' : '-translate-x-full'} md:hidden`}>
+                <ul className="list-none p-4">
+                    <li>
+                        <Link href="/about-keyboards" className="block p-2 hover:bg-gray-100">
+                            キーボードについて
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/ranking" className="block p-2 hover:bg-gray-100">
+                            ランキング
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/search-keyboards" className="block p-2 hover:bg-gray-100">
+                            キーボードを調べる
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/favorites" className="block p-2 hover:bg-gray-100">
+                            お気に入り
+                        </Link>
+                    </li>
 
-                    </div>
-                )}
+                </ul>
+            </div>
+            <div className="bg-gray-100 p-4 flex justify-center divide-x divide-gray-400 hidden md:flex">
+                <Link href="/about-keyboards" className="mx-4">
+                    About Keyboards
+                </Link>
+                <Link href="/ranking" className="mx-4">
+                    Ranking
+                </Link>
+                <Link href="/search-keyboards" className="mx-4">
+                    Search Keyboards
+                </Link>
+                <Link href="/favorites" className="mx-4">
+                    Favorites
+                </Link>
+            </div>
 
-
-            </div >
 
         </>
     );
 }
 
 export default Header;
-
-const ListItem = React.forwardRef<
-    React.ElementRef<"a">,
-    React.ComponentPropsWithoutRef<"a">
->(({ className, title, children, ...props }, ref) => {
-    return (
-        <li>
-            <NavigationMenuLink asChild>
-                <a
-                    ref={ref}
-                    className={cn(
-                        "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-                        className
-                    )}
-                    {...props}
-                >
-                    <div className="text-sm font-medium leading-none">
-                        {title}
-                    </div>
-                    <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-                        {children}
-                    </p>
-                </a>
-            </NavigationMenuLink>
-        </li>
-    );
-});
-ListItem.displayName = "ListItem";
